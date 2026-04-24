f83da0f1cf157a4ef474c0ad76559356eb4764ee
zaki <aulia.muzzaki25@gmail.com>
2026-04-24 22:40:31 +0700

    feat: ecommerce_api laravel

---
+++ ecommerce_api/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Tambahan untuk API product
+Route::resource('product', ProductController::class);
